b5076a1d6050e2afd9f03b8851e9b19b58aff9d7
Artur <onlyseooo@gmail.com>
2025-02-03 13:00:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,32 @@
-bot.py
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils import executor
+import os
+import logging
+import config
+
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot)
+
+logging.basicConfig(level=logging.INFO)
+
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.add(KeyboardButton("Растения"), KeyboardButton("Косметика"))
+keyboard.add(KeyboardButton("Ювелирные изделия"), KeyboardButton("Стекло"))
+keyboard.add(KeyboardButton("Фарфор"), KeyboardButton("Изделия из металла"))
+keyboard.add(KeyboardButton("Ввести своё"))
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Привет! Я помогу рассчитать стоимость e-commerce фотографии.\nСколько продуктов вы хотите сфотографировать?", reply_markup=types.ReplyKeyboardRemove())
+
+@dp.message_handler(lambda message: message.text.isdigit())
+async def get_products_count(message: types.Message):
+    await message.answer("Сколько фотографий нужно на один продукт?")
+
+@dp.message_handler(lambda message: message.text.isdigit())
+async def get_photos_per_product(message: types.Message):
+    await message.answer("Выберите тип продукта:", reply_markup=keyboard)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
