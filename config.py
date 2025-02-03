4875dfcb32f311584e29a29eb8e824cc1078b634
Artur <onlyseooo@gmail.com>
2025-02-03 13:34:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os
 
-TOKEN = os.getenv("7747675920:AAHRIDxJmbDofxd66weCkpqrmsnQd79g8oc") 
+TOKEN = os.getenv("7747675920:AAHRIDxJmbDofxd66weCkpqrmsnQd79g8oc")
+
+if TOKEN is None:
+    raise ValueError("🚨 BOT_TOKEN не найден! Проверь Environment Variables.")
