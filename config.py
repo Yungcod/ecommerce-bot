844038f177c9fe927519614297d4fc8823610d70
Artur <onlyseooo@gmail.com>
2025-02-03 13:43:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
-TOKEN = os.getenv("7747675920:AAHRIDxJmbDofxd66weCkpqrmsnQd79g8oc")
+TOKEN = os.getenv("7747675920:AAHRIDxJmbDofxd66weCkpqrmsnQd79g8oc")  # Загружаем токен из переменной окружения
 
-if TOKEN is None:
+if not TOKEN:
     raise ValueError("🚨 BOT_TOKEN не найден! Проверь Environment Variables.")
+
+print(f"✅ BOT_TOKEN загружен: {TOKEN[:10]}***")  # Выводит часть токена для проверки
